f3562c748bea3a180948b1b180f84a672776b2a4
vosvosvos <indvfx@gmail.com>
2022-12-10 22:09:39 +0100

    Tailwind & remove HTTPS

--- src/Elytra.Server/Program.cs
+++ src/Elytra.Server/Program.cs
@@ -1,13 +1,18 @@
 using Elytra.Server.Hubs;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
 //Response Compression Middleware
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
 
 //Enable SignalR
@@ -17,29 +22,20 @@ builder.Services.AddSignalR(o =>
 });
 
 var app = builder.Build();
-
-
+app.UseResponseCompression();
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 
-    // Might cause issues on local development, only use in prod.
+    //Causes SSL issue in dev
     app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 //Unfamiliar with what this does, maybe we can use it for hubs?
 app.MapBlazorHub();
-
 app.MapFallbackToPage("/_Host");
-
 //ChatHub
 app.MapHub<ChatHub>("/chat");
-
 app.Run();
