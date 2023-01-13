92937dcb8f84bfe53d0cef265b3bb73086dd0079
vosvosvos <indvfx@gmail.com>
2023-01-13 20:41:09 +0100

    Hub now sends back message
    Front end styling
    Hub sends back message > allows us to store messages/users etc. in database for persistance

---
+++ Elytra.Domain/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Elytra.Domain.Models
+{
+    //Just a test class
+    public class Message
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
--- src/Elytra.Server/Hubs/ChatHub.cs
+++ src/Elytra.Server/Hubs/ChatHub.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Elytra.Domain.Models;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Elytra.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(Message message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", message);
+            //Store chat in database
+            
         }
     }
 }
